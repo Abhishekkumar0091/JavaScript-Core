d3f536cfddf6d1ba042bc7a2d9c59e617bb2a2dd
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-01-17 16:22:15 +0530

    initial commit

---
+++ day3/script.js
@@ -0,0 +1,20 @@
+let heading=document.getElementById("heading");
+console.log(heading);
+
+console.log(1 + + "1");
+
+var name = "abhi";
+console.log(name);
+name="ram";
+console.log(name);
+
+let a=10;
+let b= 5;
+console.log(a%b);
+
+let a1=10;
+let a2 =20;
+console.log(a1==a2);
+
+
+
