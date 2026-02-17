e4912b1e2b60b03a7cd275050597f262ea976083
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-02-17 16:00:24 +0530

    datatype

---
+++ day6/02datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js newer version
+
+// alert( 3 + 3) //we are using nodejs, not browser
+
+console.log(3+3);
+console.log("my name is abhishek kumar")
+
+let name = "Abhishek Kumar"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null
+//undefined => 
+//Symbol => unique
+
+//object
+
+console.log(typeof "Abhishek");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
