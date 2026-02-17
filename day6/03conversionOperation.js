4257dc5498e27abe63fe616ddedbd7dddf05c190
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-02-17 16:43:19 +0530

    initial commit

---
+++ day6/03conversionOperation.js
@@ -0,0 +1,79 @@
+/*
+let score1 = 33 
+
+console.log(typeof score1);
+console.log(typeof (score1));
+*/
+
+/*
+let score2 = "33"
+
+console.log(typeof score2);
+console.log(typeof (score2));
+
+let valueInNumber = Number(score2)
+console.log(typeof valueInNumber);
+*/
+
+/*
+let score3 = "33abcd"
+
+console.log(typeof score3);
+console.log(typeof (score3));
+
+let valueInNumber = Number(score3)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/*
+let score4 = null
+
+console.log(typeof score4);
+console.log(typeof (score4));
+
+let valueInNumber = Number(score4)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+//"33" => 33
+//"33abcd" => NaN
+//true => 1; false => 0
+
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+/*
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+/*
+let isLoggedIn = "Abhishek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+//1 => true; 0 => false
+//"" => flase
+//"Abhishek => true"
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
