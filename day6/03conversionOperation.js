1a7bfe795febaad37edcbdd21d347f65c0d959b2
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-02-18 10:26:13 +0530

    intial commit

--- day6/03conversionOperation.js
+++ day6/03conversionOperation.js
@@ -35,7 +35,7 @@ console.log(typeof (score4));
 let valueInNumber = Number(score4)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
-*/
+*/ 
 
 //"33" => 33
 //"33abcd" => NaN
@@ -66,10 +66,41 @@ console.log(booleanIsLoggedIn);
 //"" => flase
 //"Abhishek => true"
 
+/*
 let someNumber =33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+//*********************Operations******************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(10%2)
+
+let str1 = "hello"
+let str2 = " Abhishek"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
+
+
+
+
+
+
+
+
+
 
 
 
